29627d3b830aa6c30b6536c170c3284ba81a69ec
novarios <novarios@nscl.msu.edu>
2018-01-23 08:19:53 -0500

    1/23 update

--- figures/CC/egas.py
+++ figures/CC/egas.py
@@ -10,17 +10,19 @@ majorLocatorY = MultipleLocator(0.05)
 minorLocatorY = MultipleLocator(0.025)
 
 filename = '/home/sam/Documents/thesis/data/Electron_Gas.dat'
+filename1 = '/home/sam/Documents/thesis/data/Electron_Gas_QMC.dat'
 
 x = [[] for i in range(5)]
 y = [[] for i in range(5)]
 
-print(filename)
+x1 = []
+y1 = []
 
 with open(filename) as f:
     data = f.read()
 data = data.split('\n')
 
-for num in range(len(data)-1):
+for num in range(len(data)):
     line = data[num].split()
     ind = (num - num%15)/15
     #print(ind, float(line[0]), float(line[6]))
@@ -28,20 +30,30 @@ for num in range(len(data)-1):
         continue
     x[ind].append(float(line[0]))
     y[ind].append(float(line[6]))
-        
+
+with open(filename1) as f1:
+    data1 = f1.read()
+data1 = data1.split('\n')
+
+for num in range(1,len(data1)):
+    line1 = data1[num].split()
+    x1.append(float(line1[0]))
+    y1.append(float(line1[1]))
+
 plt.rc('font', family='serif')
 
 fig, ax = plt.subplots()
-ax.plot(x[0], y[0], '-', marker='o', color='k', label='$\mathrm{N=14}$')
-ax.plot(x[1], y[1], '--', marker='s', color='r', label='$\mathrm{N=54}$')
-ax.plot(x[2], y[2], ':', marker='^', color='k', label='$\mathrm{N=114}$')
-ax.plot(x[3], y[3], '-', marker='o', color='r', label='$\mathrm{N=162}$')
-ax.plot(x[4], y[4], ':', marker='^', color='b', label='$\mathrm{N=246}$')
+ax.plot(x[0], y[0], ':', marker='o', color='r', label='$\mathrm{N=14}$')
+ax.plot(x[1], y[1], '-.', marker='s', color='b', label='$\mathrm{N=54}$')
+#ax.plot(x[2], y[2], '--', marker='v', color='g', label='$\mathrm{N=114}$')
+#ax.plot(x[3], y[3], '-', marker='o', color='r', label='$\mathrm{N=162}$')
+ax.plot(x[4], y[4], '-', marker='^', color='k', label='$\mathrm{N=246}$')
+ax.plot(x1, y1, '--', marker='v', color='g', label='$\mathrm{QMC}$')
 ax.legend(loc='upper right')
 
 plt.xlabel(r'$\mathrm{Wigner-Seitz\ Radius\ (r_{s})}$', fontsize=15)
 plt.ylabel(r'$\mathrm{E/A\ (Ha)}$', fontsize=15)
-plt.axis([1.0, 16.0, -0.05, 0.2])
+plt.axis([1.0, 16.0, -0.1, 0.2])
 
 #annotation_string = r'$\mathrm{25\ Shells}$'
 #annotation_string += '\n'
